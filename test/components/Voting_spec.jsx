42180f9b64d81e028495fd806927d31a81bad181
Samson Brock <SamsonLB@gmail.com>
2016-05-03 09:23:45 -0700

    Add React Pure Render Mixin.
    Enable the React Pure Render Mixin for the Voting, Vote, and Winner
    components. This indicates we intend the component to use only immutable
    data and to be written as a pure component. Pure components are checked
    for changes using a faster shallow compare rather than the default deep
    compare.

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -5,8 +5,9 @@ import {
   scryRenderedDOMComponentsWithTag,
   Simulate
 } from 'react-addons-test-utils'
+import {List} from 'immutable'
 import Voting from '../../src/components/Voting'
-import { expect } from 'chai'
+import {expect} from 'chai'
 
 describe('Voting', () => {
   it('renders a pair of buttons', () => {
@@ -67,5 +68,49 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(0)
     expect(winner).to.be.ok
     expect(winner.textContent).to.contain('Trainspotting')
+  })
+
+  it('renders as a pure component', () => {
+    const pair = ['Trainspotting', '28 Days Later']
+    const container = document.createElement('div')
+    // Simulate rendering a component
+    let component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    )
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+    expect(firstButton.textContent).to.equal('Trainspotting')
+
+    pair[0] = 'Sunshine'
+    // Simulate re-rendering a component
+    component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    )
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+    expect(firstButton.textContent).to.equal('Trainspotting')
+  })
+
+  it('does update DOM when prop changes', () => {
+    const pair = List.of('Trainspotting', '28 Days Later')
+    const container = document.createElement('div')
+    // Simulate rendering a component
+    let component = ReactDOM.render(
+      <Voting pair={pair} />,
+      container
+    )
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+    expect(firstButton.textContent).to.equal('Trainspotting')
+
+    const newPair = pair.set(0, 'Sunshine')
+    // Simulate re-rendering a component
+    component = ReactDOM.render(
+      <Voting pair={newPair} />,
+      container
+    )
+    firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+    expect(firstButton.textContent).to.equal('Sunshine')
   })
 })
