5dec34aee0d360c6bb176eee7e288ac655334bdd
Samson Brock <SamsonLB@gmail.com>
2016-05-09 17:19:34 -0700

    Connect the Redux Store to the Voting component.
    Add react-redux and connect the state to props mapping to the Voting
    component. The Voting component remains a "dumb" pure functional
    component. The "smart" component connected to the Redux Store that is
    returned by react-redux's connect() is referenced as a separate
    VotingContainer object.

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import {connect} from 'react-redux'
 import Winner from './Winner'
 import Vote from './Vote'
 
-export default React.createClass({
+// Voting is the "dumb" pure functional component.
+export const Voting = React.createClass({
   mixins: [PureRenderMixin],
   render: function () {
     // ref is used as a selector in unit tests
@@ -14,3 +16,17 @@ export default React.createClass({
     </div>
   }
 })
+
+function mapStateToProps (state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  }
+}
+
+connect(mapStateToProps)(Voting)
+
+// VotingContainer is the "smart" connected Voting component. It wraps the
+// "dumb" Voting component with react-redux logic that keeps it in sync with the
+// Redux Store.
+export const VotingContainer = connect(mapStateToProps)(Voting)
